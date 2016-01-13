a88c253979b63f509207ab8d77a68c2c0d724af6
myThemero <theotibo@hotmail.fr>
2016-01-13 21:48:38 +0100

    I made the header of Draw.

---
+++ src/Draw.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Théo on 13/01/2016.
+//
+
+#include "Draw.h"
---
+++ src/Draw.h
@@ -0,0 +1,41 @@
+//
+// Created by Théo on 13/01/2016.
+//
+
+#ifndef TP_CPP_5_DRAW_H
+#define TP_CPP_5_DRAW_H
+
+#include "Object.h"
+#include <string>
+#include <map>
+#include <list>
+using namespace std;
+
+class Draw
+{
+
+//Public methods
+public:
+    int AddSegment(string name, string points);
+    int AddPolygon(string name, string points);
+    int AddRectangle(string name, string points);
+    int List();
+    int Move(string object, int dX, int dY);
+    int Clear();
+    int Save(string filename);
+    int Load(string filename);
+    int Undo();
+    int Redo();
+
+    //Constructor
+    Draw();
+
+protected:
+    map<string,Object> allObjects;
+    list<string> historic;
+    list<string> reverseHistoric;
+    int historicPosition;
+
+};
+
+#endif //TP_CPP_5_DRAW_H
