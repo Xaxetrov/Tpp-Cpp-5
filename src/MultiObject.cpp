3242e7a43d84c673bce48bab2a76cca5f11e10f6
myThemero <theotibo@hotmail.fr>
2016-01-29 18:48:55 +0100

    MultiObject is functional ! Yeepee

---
+++ src/MultiObject.cpp
@@ -0,0 +1,74 @@
+//
+// Created by Théo on 29/01/2016.
+//
+
+// ------------------------------------------------------------------------------------------------------------ INCLUDES
+// Personal includes
+#include "MultiObject.h"
+// System includes
+#include <string>
+#include <list>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+// ------------------------------------------------------------------------------------------------------ CONSTRUCTOR(S)
+MultiObject::MultiObject(string aName, list<Object *> objectsToUse)
+    : Object(aName)
+{
+    list<Object *>::iterator i;
+    for(i = objectsToUse.begin(); i != objectsToUse.end(); i++)
+    {
+        composingObjects.push_back(*i);
+    }
+}
+
+MultiObject::~MultiObject()
+{
+    list<Object *>::iterator i;
+    for(i = composingObjects.begin(); i != composingObjects.end(); i++)
+    {
+        delete *i;
+    }
+}
+
+// ------------------------------------------------------------------------------------------------------------- METHODS
+string MultiObject::toString()
+{
+    stringstream toReturn;
+    toReturn << "Composed object named ";
+    toReturn << name;
+    toReturn << " from ";
+    toReturn << composingObjects.size();
+    toReturn << " objects.";
+
+    string result;
+    getline(toReturn,result);
+    return result;
+}
+
+const bool MultiObject::Hits(Point aPoint)
+{
+    return false;
+}
+
+int MultiObject::Move(int dX, int dY)
+{
+    list<Object *>::iterator i;
+    int result = 0;
+
+    for(i=composingObjects.begin();i != composingObjects.end();i++)
+    {
+        if(!((*i)->Move(dX,dY)))
+        {
+            result=3;
+        }
+    }
+
+    return result;
+}
+
+int MultiObject::GetCommand(ostream &os)
+{
+    return 0;
+}
---
+++ src/MultiObject.h
@@ -0,0 +1,53 @@
+//
+// Created by Théo on 29/01/2016.
+//
+
+#ifndef TP_CPP_5_MULTIOBJECT_H
+#define TP_CPP_5_MULTIOBJECT_H
+
+// ------------------------------------------------------------------------------------------------------------ Includes
+// Personal includes
+#include "Object.h"
+// System includes
+#include <string>
+#include <list>
+#include <iostream>
+using namespace std;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// The class Multiobject represent an object which have been made by an union or an intersection
+// ---------------------------------------------------------------------------------------------------------------------
+
+class MultiObject : public Object
+{
+
+public:
+    // ---------------------------------------------------------------------------------------------------------- PUBLIC
+    // Public methods
+
+    virtual const bool Hits(Point aPoint)=0;
+    // This method return true if the point hits the multiobject
+
+    virtual int Move(int dX, int dY);
+    // This method will move the multiobject by the vector enter in parameters
+
+    virtual string toString();
+    // This method give a representation of the multiobject in a string
+
+    virtual int GetCommand(ostream &os)=0;
+    // This method give the command that permits to make the multiobject
+
+    //Constructor(s)
+    MultiObject(string aName, list<Object *> objectsToUse);
+    // This method create a multiobject based on other objects.
+
+    virtual ~MultiObject();
+    // Destructor of a MultiObject
+
+    // ------------------------------------------------------------------------------------------------------- PROTECTED
+    // Attributes
+    list<Object *> composingObjects;
+
+};
+
+#endif //TP_CPP_5_MULTIOBJECT_H
