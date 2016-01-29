0577f771e7e4c5cb846c75d9d0cca502e0094cf2
myThemero <theotibo@hotmail.fr>
2016-01-29 19:28:03 +0100

    Intersection is alright but the GetCommand is missing :)

---
+++ src/Intersection.cpp
@@ -0,0 +1,63 @@
+//
+// Created by Théo on 29/01/2016.
+//
+
+// ------------------------------------------------------------------------------------------------------------ INCLUDES
+// Personal includes
+#include "Intersection.h"
+// System includes
+#include <string>
+#include <list>
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+// ------------------------------------------------------------------------------------------------------ CONSTRUCTOR(S)
+Intersection::Intersection(string name, list<Object*> objectsToUse)
+        : MultiObject(name,objectsToUse)
+{
+
+}
+
+Intersection::~Intersection()
+{
+
+}
+
+// ------------------------------------------------------------------------------------------------------------- METHODS
+const bool Intersection::Hits(Point aPoint)
+{
+    bool result=true;
+    list<Object *>::iterator i;
+
+    for(i=composingObjects.begin(); i != composingObjects.end(); i++)
+    {
+        // If one of the object of the intesection isn't hit, the intersection is not hitten !
+        if(!((*i)->Hits(aPoint)))
+        {
+            result=false;
+        }
+    }
+
+    return result;
+}
+
+string Intersection::toString()
+{
+    stringstream toReturn;
+    toReturn << "Intersection named ";
+    toReturn << name;
+    toReturn << " from ";
+    toReturn << composingObjects.size();
+    toReturn << " objects.";
+
+    string result;
+    getline(toReturn,result);
+    return result;
+}
+
+int Intersection::GetCommand(ostream &os)
+{
+    return 0; //TODO : This.
+}
+
---
+++ src/Intersection.h
@@ -0,0 +1,42 @@
+//
+// Created by Théo on 29/01/2016.
+//
+
+#ifndef TP_CPP_5_INTERSECTION_H
+#define TP_CPP_5_INTERSECTION_H
+
+// ------------------------------------------------------------------------------------------------------------ INCLUDES
+// Personal includes
+#include "MultiObject.h"
+// System includes
+#include <string>
+#include <iostream>
+#include <list>
+using namespace std;
+
+// ---------------------------------------------------------------------------------------------------------------------
+// The class Intersection represents an intersection of few objects
+// ---------------------------------------------------------------------------------------------------------------------
+
+class Intersection : public MultiObject
+{
+
+public:
+    // ---------------------------------------------------------------------------------------------------------- PUBLIC
+    const bool Hits(Point aPoint);
+    // This method return true if the point hits all objects of the multiobject intersection
+
+    string toString();
+    // This method give a representation of the intersection in a string
+
+    int GetCommand(ostream &os);
+    // This method give the command that permits to make the intersection
+
+    // Constructor(s)
+    Intersection(string name, list<Object *> objectsToUse);
+    virtual ~Intersection();
+
+};
+
+
+#endif //TP_CPP_5_INTERSECTION_H
